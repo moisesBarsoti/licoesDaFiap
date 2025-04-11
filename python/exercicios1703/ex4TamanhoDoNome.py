f422acbdaf69b40f85e2af2dee88321d51ac3962
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2025-04-11 16:14:51 -0300

    Delete python directory

--- python/exercicios1703/ex4TamanhoDoNome.py
+++
@@ -1,8 +0,0 @@
-"""
-4. Mostrar o tamanho de um texto.
-Peça ao usuário que digite seu nome.
-Informe quantas letras tem o nome fornecido.
-"""
-
-nome = input("Digite o seu nome: ");
-print(f"O nome {nome} tem essa quantidade de letras: {len(nome)}");
--- python/exercicios2603/saldoDoBanco.py
+++
@@ -1,6 +0,0 @@
-saldo = float(input("Qual é o seu saldo no banco: "));
-
-if saldo > 0:
-    print(f"O seu saldo é de: R${saldo}");
-else:
-    print("O seu saldo é de R$0");
